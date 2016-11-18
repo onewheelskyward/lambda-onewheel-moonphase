3ca1a43557226cef52f017a29c80c89f1df47bff
Andrew Kreps <andrew.kreps@gmail.com>
2016-11-17 20:31:39 -0800

    Returning basic data, still some maths to do but all good

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Lock into the moon data right here: http://api.usno.navy.mil/rstt/oneday?date=11/12/2016&coords=35.3030433,-106.4350902&tz=-7
 
+# Docs
+
+http://aa.usno.navy.mil/data/docs/api.php
+
 # Accessing
 /moon?loc=[string]
 
--- moonphase/index.js
+++ moonphase/index.js
@@ -29,13 +29,45 @@ exports.handler = function (event, context) {
             superagent.get(timezoneUri + "lat=" + lat + "&lng=" + lng)
                 .end(function (err, res) {
                     console.log("Timezone: " + res.body);
-                    console.log("latLng" + lat + " " + lng);
+                    console.log("latLng: " + lat + " " + lng);
                     superagent.get(moonUri + "date=" + dateStr + "&coords=" + lat + "," + lng + "&tz=" + res.body)
                         .end(function (err, res) {
+                            console.log(res.body);
+                            var response = {
+                                year: res.body.year,
+                                month: res.body.month,
+                                day: res.body.day,
+                                dayOfWeek: res.body.dayofweek,
+                                lng: res.body.lon,
+                                lat: res.body.lat,
+                                tz: res.body.tz,
+                                sundata: {
+                                    civilRise: res.body.sundata[0].time,
+                                    rise: res.body.sundata[1].time,
+                                    transit: res.body.sundata[2].time,
+                                    set: res.body.sundata[3].time,
+                                    civilSet: res.body.sundata[4].time
+                                },
+                                moondata: {}
+                            };
+
+                            res.body.moondata.forEach(function(moondatum) {
+                                switch(moondatum.phen) {
+                                    case "R":
+                                        response.moondata.rise = moondatum.time;
+                                        break;
+                                    case "U":
+                                        response.moondata.transit = moondatum.time;
+                                        break;
+                                    case "S":
+                                        response.moondata.set = moondatum.time;
+                                        break;
+                                };
+                            });
                             context.succeed({
                                 statusCode: 200,
                                 headers: {},
-                                body: JSON.stringify(res.body)
+                                body: JSON.stringify(response)
                             });
                         });
                 });
